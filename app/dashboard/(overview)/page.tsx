ab60d2680a026d3375528a3a4f7368165859b475
Roshan9292 <103491108+roshan9292@users.noreply.github.com>
2024-07-24 18:28:46 +0545

    first commit

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -22,11 +22,6 @@ export default async function Page() {
         <Suspense fallback={<CardSkeleton/>}>
           <CardWrapper />
         </Suspense>
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback ={<RevenueChartSkeleton/>}>
